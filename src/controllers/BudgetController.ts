2c9b1724c428178f401f156eea48a80fc5bc03fa
brahanlider <brahanlidertunquipamamani@gmail.com>
2025-03-31 17:57:06 -0500

    Add rutas y metodos al Controller

--- src/controllers/BudgetController.ts
+++ src/controllers/BudgetController.ts
@@ -2,10 +2,23 @@ import type { Request, Response } from "express";
 
 export class BudgetController {
   static getAll = async (req: Request, res: Response) => {
-    console.log("Desde GETALL /api/budgets");
+    console.log("Desde getAll /api/budgets");
+  };
+
+  static getById = async (req: Request, res: Response) => {
+    console.log("Desde getById /api/budgets/id");
   };
 
   static create = async (req: Request, res: Response) => {
-    console.log("asdasdas Desde POST /api/budgets");
+    console.log(" Desde create /api/budgets");
   };
+
+  static updateById = async (req: Request, res: Response) => {
+    console.log(" Desde updateById /api/budgets");
+  };
+
+  static deleteById = async (req: Request, res: Response) => {
+    console.log(" Desde delete /api/budgets");
+  };
+  
 }
--- src/routes/budgetRouter.ts
+++ src/routes/budgetRouter.ts
@@ -4,6 +4,9 @@ import { BudgetController } from "../controllers/BudgetController";
 const router = Router();
 
 router.get("/", BudgetController.getAll);
+router.get("/:id", BudgetController.getById);
 router.post("/", BudgetController.create);
+router.put("/:id", BudgetController.updateById);
+router.delete("/:id", BudgetController.deleteById);
 
 export default router;
