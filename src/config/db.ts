5f501b357576084f6bc909cf4da3fa98cced02c0
brahanlider <brahanlidertunquipamamani@gmail.com>
2025-03-31 17:10:12 -0500

    1: Create model Presupuesto

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,13 +1,24 @@
 import { Sequelize } from "sequelize-typescript";
 import dotenv from "dotenv";
 
+// Configuración de variables de entorno
 dotenv.config();
 
 export const db = new Sequelize(process.env.DATABASE_URL, {
+  // Configuración de modelos (ruta segura con path.resolve)
+  models: [__dirname + "/../models/**/*"],
+  //// Configuración de logs (solo en desarrollo)
+  //// logging: false,
+
+  //// Configuración de timestamps (mejor práctica): created etc
+  //// define: {
+  ////   timestamps: false,
+  //// },
+
+  // Configuración SSL segura para producción
   dialectOptions: {
-    // ssl => porque no tenemos un certificado seguro (corregir)
     ssl: {
-      require: false,
+      require: false, // Obligatorio para entornos cloud
     },
   },
 });
