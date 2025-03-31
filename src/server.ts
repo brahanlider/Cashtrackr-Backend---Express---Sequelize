ebb914aa5685274ed531cc4715ec9e76989e1510
brahanlider <brahanlidertunquipamamani@gmail.com>
2025-03-31 17:31:31 -0500

    Router connect server

--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,7 @@ import express from "express";
 import colors from "colors";
 import morgan from "morgan";
 import { db } from "./config/db";
-
+import budgetRouter from "./routes/budgetRouter";
 async function connectDB() {
   try {
     await db.authenticate(); // ya podemos hacer operacione en la bd
@@ -23,6 +23,8 @@ const app = express();
 app.use(morgan("dev"));
 
 app.use(express.json());
+
+app.use("/api/budgets", budgetRouter);
 
 export default app;
 // const express = require("express");
