e36af9f57df821bbc135b37b8b7496b69fa1788a
brahanlider <brahanlidertunquipamamani@gmail.com>
2025-04-01 14:08:08 -0500

    create controllers and routers validations

--- src/controllers/BudgetController.ts
+++ src/controllers/BudgetController.ts
@@ -1,24 +1,85 @@
 import type { Request, Response } from "express";
+import Budget from "../models/Budget";
 
 export class BudgetController {
   static getAll = async (req: Request, res: Response) => {
-    console.log("Desde getAll /api/budgets");
+    try {
+      const budgets = await Budget.findAll({
+        order: [
+          ["createdAt", "DESC"], // ← Formato correcto: array de arrays
+        ],
+        // limit: 1,
+        //whgere:{}
+      });
+      res.status(200).json(budgets);
+    } catch (error) {
+      res.status(500).json({ error: "Error al obtener los presupuestos" });
+    }
   };
 
   static getById = async (req: Request, res: Response) => {
-    console.log("Desde getById /api/budgets/id");
+    try {
+      const { id } = req.params;
+      const budgetId = await Budget.findByPk(id);
+
+      if (!budgetId) {
+        const error = new Error("Presupuesto no encontrado");
+        res.status(404).json({ error: error.message });
+        return;
+      }
+
+      res.status(200).json(budgetId);
+    } catch (error) {
+      res.status(500).json({ error: "Error al obtener un producto por Id" });
+    }
   };
 
   static create = async (req: Request, res: Response) => {
-    console.log(" Desde create /api/budgets");
+    try {
+      const budget = new Budget(req.body);
+
+      await budget.save();
+      res.status(201).json("Presupuesto Creado Correctamente");
+    } catch (error) {
+      res.status(500).json({ error: "Hubo un eror" });
+    }
   };
 
   static updateById = async (req: Request, res: Response) => {
-    console.log(" Desde updateById /api/budgets");
+    const { id } = req.params;
+    const { name, amount } = req.body;
+    try {
+      const budget = await Budget.findByPk(id);
+
+      if (!budget) {
+        const error = new Error("Presupuesto no encontrado");
+        res.status(404).json({ error: error.message });
+        return;
+      }
+
+      await budget.update({ name, amount });
+
+      res.status(200).json(budget);
+    } catch (error) {
+      res.status(500).json({ error: "Hubo un error al editar" });
+    }
   };
 
   static deleteById = async (req: Request, res: Response) => {
-    console.log(" Desde delete /api/budgets");
+    try {
+      const { id } = req.params;
+      const budget = await Budget.findByPk(id);
+
+      if (!budget) {
+        const error = new Error("Presupuesto no encontrado");
+        res.status(404).json({ error: error.message });
+        return;
+      }
+      await budget.destroy();
+
+      res.status(200).json("Presupuesto eliminado correctamente");
+    } catch (error) {
+      res.status(500).json({ error: "Hubo un error al eliminar" });
+    }
   };
-  
 }
--- src/routes/budgetRouter.ts
+++ src/routes/budgetRouter.ts
@@ -1,12 +1,72 @@
 import { Router } from "express";
+import { body, param } from "express-validator";
 import { BudgetController } from "../controllers/BudgetController";
+import { handleInputErrors } from "../middleware/validation";
+import Budget from "../models/Budget";
 
 const router = Router();
 
 router.get("/", BudgetController.getAll);
-router.get("/:id", BudgetController.getById);
-router.post("/", BudgetController.create);
-router.put("/:id", BudgetController.updateById);
-router.delete("/:id", BudgetController.deleteById);
+router.get(
+  "/:id",
+  param("id")
+    .isInt({ min: 1 })
+    .withMessage("El ID debe ser un número entero positivo"),
+
+  handleInputErrors,
+  BudgetController.getById
+);
+router.post(
+  "/",
+  body("name")
+    .notEmpty()
+    .withMessage("El nombre del presupuesto es obligatorio"),
+  body("amount")
+    .notEmpty()
+    .withMessage("La cantidad del presupuesto no puede ir vacio")
+    .isNumeric()
+    .withMessage("Cantidad no válida")
+    .custom((value) => value > 0)
+    .withMessage("El presupuesto debe ser mayor a 0"),
+
+  handleInputErrors,
+  BudgetController.create
+);
+router.put(
+  "/:id",
+  param("id")
+    .isInt({ min: 1 })
+    .withMessage("El ID debe ser un número entero positivo"),
+
+  body("name")
+    .trim()
+    .notEmpty()
+    .withMessage("El nombre del presupuesto es obligatorio"),
+
+  body("amount")
+    .notEmpty()
+    .withMessage("La cantidad no puede estar vacía")
+    .isFloat({ min: 0.01 }) // Acepta decimales y valida tipo number
+    .withMessage("Debe ser un número válido mayor a 0")
+    .custom((value) => {
+      if (typeof value !== "number") {
+        throw new Error("Debe ser un número (no texto)");
+      }
+      return true;
+    }),
+
+  handleInputErrors,
+  BudgetController.updateById
+);
+router.delete(
+  "/:id",
+  param("id")
+    .isInt()
+    .withMessage("El ID debe ser un número entero"),
+
+
+  handleInputErrors,
+  BudgetController.deleteById
+);
 
 export default router;
