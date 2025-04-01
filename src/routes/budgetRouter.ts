80165379ff3a8adc45a37f3a3616c0874cb2ce97
brahanlider <brahanlidertunquipamamani@gmail.com>
2025-04-01 15:45:39 -0500

    validate with params AND middleware express-validator

--- src/routes/budgetRouter.ts
+++ src/routes/budgetRouter.ts
@@ -3,65 +3,31 @@ import { body, param } from "express-validator";
 import { BudgetController } from "../controllers/BudgetController";
 import { handleInputErrors } from "../middleware/validation";
 import Budget from "../models/Budget";
-import { validateBudgetExists, validateBudgetId } from "../middleware/budget";
+import {
+  validateBudgetExists,
+  validateBudgetId,
+  validateBudgetInput,
+} from "../middleware/budget";
 
 const router = Router();
 
+router.param("budgetId", validateBudgetId);
+router.param("budgetId", validateBudgetExists);
+
 router.get("/", BudgetController.getAll);
-router.get(
-  "/:id",
-  validateBudgetId,
-  validateBudgetExists,
-  BudgetController.getById
-);
+router.get("/:budgetId", BudgetController.getById);
 router.post(
   "/",
-  body("name")
-    .notEmpty()
-    .withMessage("El nombre del presupuesto es obligatorio"),
-  body("amount")
-    .notEmpty()
-    .withMessage("La cantidad del presupuesto no puede ir vacio")
-    .isNumeric()
-    .withMessage("Cantidad no válida")
-    .custom((value) => value > 0)
-    .withMessage("El presupuesto debe ser mayor a 0"),
-
+  validateBudgetInput,
   handleInputErrors,
   BudgetController.create
 );
 router.put(
-  "/:id",
-  validateBudgetId,
-  validateBudgetExists,
-
-  body("name")
-    .trim()
-    .notEmpty()
-    .withMessage("El nombre del presupuesto es obligatorio"),
-
-  body("amount")
-    .notEmpty()
-    .withMessage("La cantidad no puede estar vacía")
-    .isFloat({ min: 0.01 }) // Acepta decimales y valida tipo number
-    .withMessage("Debe ser un número válido mayor a 0")
-    .custom((value) => {
-      if (typeof value !== "number") {
-        throw new Error("Debe ser un número (no texto)");
-      }
-      return true;
-    }),
-
+  "/:budgetId",
+  validateBudgetInput,
   handleInputErrors,
   BudgetController.updateById
 );
-router.delete(
-  "/:id",
-  validateBudgetId,
-  validateBudgetExists,
-
-  handleInputErrors,
-  BudgetController.deleteById
-);
+router.delete("/:budgetId", BudgetController.deleteById);
 
 export default router;
